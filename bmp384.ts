82d89d521e62533cade3d5c04f1672236a56e694
townengineering <wyatt@townschool.com>
2025-08-28 11:29:21 -0700

    Update bmp384.ts

--- bmp384.ts
+++ bmp384.ts
@@ -1,8 +1,5 @@
 //% weight=95 color=#2E86C1 icon="\uf135" block="BMP384"
 namespace bmp384 {
-    /**
-     * Say hello on the LED display.
-     */
     //% block="BMP384 hello"
     export function hello(): void {
         basic.showString("BMP384")
